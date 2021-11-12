91e3fa43c50978d4acc0b01fc77e151dd7dec01f
Michael <mikemountain.dev@gmail.com>
2021-11-12 19:29:54 +0200

    initial commit

---
+++ apps/documentation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'infinity-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'documentation';
+}
